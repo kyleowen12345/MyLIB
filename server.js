1e9c285bdf9d9bcef73a0a8de0c0a9ccb08aaa49
Kyle Owen Ga <gakyleowen@gmail.com>
2020-07-14 13:08:53 +0800

    episode2

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const mongoose = require("mongoose");
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
 
 dotenv.config();
 // Configurations
+
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 app.use(expressLayouts);
 app.use(express.static("public"));
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 // Connect to database
 mongoose.connect(
